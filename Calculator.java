b8582617d4aa02fa6c184ea3e4b1bd0fa4ee8a79
saimkhan22 <saimalikhan2019@gmail.com>
2025-02-08 17:20:21 +0500

    Added modulus and exponentiation features

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter an operator (+, -, *, /, %, ^): ");
+        char operator = scanner.next().charAt(0);
+
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        switch (operator) {
+            case '+': result = num1 + num2; break;
+            case '-': result = num1 - num2; break;
+            case '*': result = num1 * num2; break;
+            case '/': result = num2 != 0 ? num1 / num2 : Double.NaN; break;
+            case '%': result = num1 % num2; break;
+            case '^': result = Math.pow(num1, num2); break;
+            default: System.out.println("Invalid operator!"); return;
+        }
+
+        System.out.println("Result: " + result);
+        scanner.close();
+    }
+}
